d4c5a94b0cc195e15e3a67b3e68e5de36227cc30
CataPutaAPP <cataputaapp@gmail.com>
2026-09-04 12:32:08 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,1 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_URL = "https://xnlilijaxevnlueekwzw.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhubGlsaWpheGV2bmx1ZWVrd3p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1Mjg0MDcsImV4cCI6MjEwNDEwNDQwN30.4Xqc2bQ-bkuI_fCS-PhnaYy4vrpDyOMn1snWCgjUHbQ";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
